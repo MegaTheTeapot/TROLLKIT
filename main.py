a45f051bc7f3bd5d9bb3d8afb11fc3f21b6d6b88
mega145 <kacpermi@poczta.fm>
2021-05-17 10:04:49 +0200

    added crash command

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# Made by Mega145
+# github.com/mega145
+# original repository:
+# github.com/mega145/TROLLKIT
+
+# If you made a fork of this dont delete the above
+
+
 import os,json
 import time
 import discord
@@ -21,15 +29,20 @@ def leave(error=None):
     
     print('Ended!')
 
+buglist = "BUGS:\nPlay command doesnt work on the exe file (source file works)"
+commands_list = "Commands:\n- play - Plays an audio file in your current vc (works with mp4's too)\n- spam - Sends contents of txt file in assets/texts (you can also specify the delay)\n- crash - sends a crash gif/video from assets/crash"
 
 @client.event
 async def on_ready():
     print(f"{ascii_banner}____________________________________________\n\n")
     print(f"{client.user.name}#{client.user.discriminator} Ready to serve (prefix: {prefix})\n")
+    print(commands_list)
+    print(buglist)
 
 
-
-@client.command(pass_context=True)
+#Play command not working properly in the exe file
+#TODO: Make pynacl library import properly in the exe
+@client.command(pass_context=True,description="Plays an audio file in your current vc (works with mp4's too)")
 async def play(ctx,media=None):
     global ch
 
@@ -44,7 +57,7 @@ async def play(ctx,media=None):
         voice.play(src,after=lambda error: leave())
 
 @client.command(pass_context=True)
-async def say(ctx,filename,delay=0.7):
+async def spam(ctx,filename,delay=0.7):
     f = open(f'assets/texts/{filename}')
     await ctx.message.delete()
     print(f'saying contents of {filename}')
@@ -56,8 +69,11 @@ async def say(ctx,filename,delay=0.7):
         time.sleep(delay)
     print('Ended!')
 
-
-
+@client.command(pass_context=True,description='sends a crash gif/video (you can use a custom one in the assets folder)')
+async def crash(ctx,filename : str):
+    await ctx.message.delete()
+    file = discord.File(f'assets/crash/{filename}')
+    await ctx.send(file=file)
 
 try:
     client.run(token,bot=False)
